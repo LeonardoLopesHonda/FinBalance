2f63ec33144fc60319ba34d69fa00d4c5685ed6f
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-01-19 23:53:28 -0400

    Added `database.js` with `.env` configs

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,16 @@
-function status(request, response) {
-    response.status(200).json({
-        field_1: "Testing 1",
-        field_2: {
-          sub_field: "subfield content são",
-        },
-    });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1;');
+
+  console.log(result);
+
+  response.status(200).json({
+      field_1: "Testing 1",
+      field_2: {
+        sub_field: "subfield content são",
+      },
+  });
 }
 
 export default status;
