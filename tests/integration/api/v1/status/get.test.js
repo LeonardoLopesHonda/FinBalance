84bac78dd316322dddd4e0a284f59b707ca73497
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-01-26 23:33:55 -0400

    Added tests to `status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,10 @@
 test("GET in /api/v1/status should return 200", async () => {
     const response = await fetch("http://localhost:3000/api/v1/status")
+    const responseBody = response.json();
 
     expect(response.status).toBe(200);
+    expect(responseBody.updatedAt).toBeDefined();
+    expect(responseBody.dependencies.database.version).toBeDefined();
+    expect(responseBody.dependencies.database.max_connections).toBeDefined();
+    expect(responseBody.dependencies.database.opened_connections).toBeDefined();
 });
