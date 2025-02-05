f8dfa0e3d4d719ad1c2fbf048e8f8c5e4270d78c
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-02-05 00:31:40 -0400

    Adds `/api/v1/migrations` endpoint

---
+++ tests/integration/api/v1/migrations/post.test.js
@@ -0,0 +1,26 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+    await database.query("DROP SCHEMA public cascade; CREATE SCHEMA public;");
+}
+
+test("POST in /api/v1/migrations should return 200", async () => {
+    const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+    });
+    const responseBody = await response.json();
+    expect(response.status).toBe(201);
+    expect(Array.isArray(responseBody)).toBe(true);
+    expect(responseBody.length).toBeGreaterThan(0);
+
+    // Testing if migrations were executed
+    const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "POST",
+    });
+    const response2Body = await response2.json();
+    expect(response2.status).toBe(200);
+    expect(Array.isArray(response2Body)).toBe(true);
+    expect(response2Body.length).toBe(0);
+});
