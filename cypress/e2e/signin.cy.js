de502d7349255086732d24fc126061f303657388
Leonardo Lopes Honda <leozinhohondams@gmail.com>
2025-07-20 16:43:37 -0400

    refactor: change file name to more coherent

---
+++ cypress/e2e/signin.cy.js
@@ -0,0 +1,24 @@
+/* eslint-disable jest/expect-expect */
+describe("Signin login with Credentials", () => {
+  it("Sucessfully Signin with correct Credentials", () => {
+    cy.visit("http://localhost:3000/status");
+
+    cy.get("input[name='email']").type("honda@gmail.com");
+    cy.get("input[name='password']").type("senha123");
+
+    cy.get(":nth-child(2) > form > button").click();
+
+    cy.url().should("include", "/status");
+  });
+
+  it("Failed Signin with incorrect Credentials", () => {
+    cy.visit("http://localhost:3000/status");
+
+    cy.get("input[name='email']").type("emailtodoerrado@gmail.com");
+    cy.get("input[name='password']").type("senhatodaerradatambém");
+
+    cy.get(":nth-child(2) > form > button").click();
+
+    cy.url().should("include", "/api/auth/error");
+  });
+});
